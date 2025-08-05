45e21ab67c282a75dd0de451c22396e7a3366472
Bruna Pessoa <brunapessoa.bpm@gmail.com>
2025-08-05 09:41:12 +0200

    development of dictionay in portuguese, english and duth

---
+++ content.js
@@ -0,0 +1,57 @@
+export const titlePtbr = 'Desenvolvedora de Software Front-End'
+
+export const aboutMePtBr = 'Sobre mim'
+export const skillsPtBr = 'Competências'
+export const projectsPtBr = 'Projetos'
+export const coursesPtBr = 'Cursos'
+export const networkPtBr = 'Redes Sociais'
+
+export const aboutTxtPtBr = '<p>Sou brasileira, vivendo em Eindhoven desde abril de 2023. Durante esse período, tenho me dedicado a aprimorar meu inglês e recentemente comecei a estudar holandês para facilitar nossa integração aos Países Baixos.</p><p>Atualmente estou estudando Desenvolvimento Web, aprendendo e aprimorando competências técnicas que me permitirão ingressar no mercado de tecnologia. Até o momento, estudei linguagens de programação e frameworks como HTML, CSS e JavaScript.</p>'
+
+export const projectsTxtPtBr = 'Você pode acessar os meus projetos:'
+
+export const androidPagePtBr = 'Página Android'
+
+export const coursesTxtPtBr = '<li>Trybe - Escola de Tecnologia</li><li>Curso em Vídeo - por Gustavo Guanabara:<ul><li>Módulo: HTML & CSS</li><li>Módulo: JavaScript</li></ul></li>'
+
+export const footerPtBr = 'Bruna Pessoa Machado - Desenvolvedora de Software'
+/* ====================================================== */
+
+export const titleEng = 'Front-end Software Developer'
+
+export const aboutMeEng = 'About me'
+export const skillsEng = 'Skills'
+export const projectsEng = 'Projects'
+export const coursesEng = 'Courses'
+export const networkEng = 'Network'
+
+export const aboutTxtEng = "<p>I'm a Brazilian citizen, living in Eindhoven since April 2023. During this period, I have been dedicated to improving my English and have recently started studying Dutch to facilitate our integration into the Netherlands.</p><p>Currently studying Web Development, learning and improving hard skills that will enable me to enter the technology market. So far, programming languages and frameworks such as HTML, CSS, JavaScript have been studied.</p>"
+
+export const projectsTxtEng = 'You can acess my projects`s  pages:'
+
+export const androidPageEng = 'Android Page'
+
+export const coursesTxtEng = '<li>Trybe - Technology School</li><li>Curso em Vídeo by Gustavo Guanabara:<ul><li>Module: HTML & CSS</li><li>Module: JavaScript</li></ul></li>'
+
+export const footerEng = 'Bruna Pessoa Machado - Software Developer'
+
+/* ======================================================== */
+
+export const titleNl = 'Front-end Softwareontwikkelaar'
+
+export const aboutMeNl = 'Over mij'
+export const skillsNl = 'Vaardigheden'
+export const projectsNl = 'Projecten'
+export const coursesNl = 'Cursussen'
+export const networkNl = 'Netwerk'
+
+export const aboutTxtNl = '<p>Ik ben een Braziliaanse burger en woon sinds april 2023 in Eindhoven. In deze periode heb ik mij toegewijd aan het verbeteren van mijn Engels en ben ik onlangs begonnen met het leren van Nederlands om onze integratie in Nederland te vergemakkelijken.</p><p>Momenteel studeer ik Web Development, waarbij ik hard skills leer en verbeter die mij in staat zullen stellen om de technologiesector te betreden. Tot nu toe heb ik programmeertalen en frameworks zoals HTML, CSS en JavaScript bestudeerd.</p>'
+
+export const projectsTxtNl = 'Je kunt de pagina`s van mijn projecten bekijken:'
+
+export const androidPageNl = 'Android-pagina'
+
+export const coursesTxtNl = '<li>Trybe - Technology School</li><li>Curso em Vídeo door Gustavo Guanabara:<ul><li>Module: HTML & CSS</li><li>Module: JavaScript</li></ul></li>'
+
+
+export const footerNl = 'Bruna Pessoa Machado – Softwareontwikkelaar'
