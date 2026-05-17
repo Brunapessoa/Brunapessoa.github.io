9819d69b6c71168fa7c191e244a3e381161718ef
Bruna Pessoa <brunapessoa.bpm@gmail.com>
2026-05-17 20:36:38 +0200

    mobile style changed

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,26 +1,36 @@
 const menuItems = document.getElementById("menuItems");
+const burger = document.getElementById("burger");
 
 function clickMenu() {
-    const menuItems = document.getElementById("menuItems");
-    
-    if (menuItems.style.display == 'block') {
-        menuItems.style.display = 'none'
-    }
-    else {
-        menuItems.style.display = 'block'
-    }
+  const menuItems = document.getElementById("menuItems");
+
+  if (menuItems.style.width == "50%") {
+    menuItems.style.width = "0";
+  } else {
+    menuItems.style.width = "50%";
+  }
 }
 
 function biggerScreen() {
-    if (window.innerWidth >= 992) {
-        menuItems.style.display = 'block'
-    } else {
-        menuItems.style.display = 'none'
-    }
+  if (window.innerWidth >= 992) {
+    menuItems.style.width = "50%";
+  } else {
+    menuItems.style.width = "0";
+  }
 }
 
-biggerScreen() 
+biggerScreen();
 
+const selectedLink = document.querySelectorAll("#menuItems a");
 
+selectedLink.forEach(function (link) {
+  link.addEventListener("click", function () {
+    menuItems.style.width = "0";
+  });
+});
 
-
+document.addEventListener("click", function (event) {
+  if (!menuItems.contains(event.target) && !burger.contains(event.target)) {
+    menuItems.style.width = "0";
+  }
+});
